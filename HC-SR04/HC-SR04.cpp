321405869776ba5696c2b7b6ba16ffb652fbcdbd
halina20011 <65724763+halina20011@users.noreply.github.com>
2021-07-22 12:41:18 +0200

    Add files via upload

---
+++ HC-SR04/HC-SR04.cpp
@@ -0,0 +1,33 @@
+/*#################################################################################
+ * This library was produced by Halina
+ * 
+ *  github: https://github.com/halina20011/HC-SR04-Libraries
+###################################################################################*/
+
+#include "Arduino.h"
+#include "HC-SR04.h"
+
+HCSR04::HCSR04(int echoPin,int trigPin){
+ echoPIN = echoPin;
+ trigPIN = trigPin;
+ pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);
+}
+
+HCSR04::showDistance(bool cm){
+  long duration; 
+  float distance;
+  digitalWrite(trigPIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPIN, LOW);
+  duration = pulseIn(echoPIN, HIGH);
+  distance = duration * 0.034 / 2; 
+  if(cm == true){
+  	return distance;	
+  }
+  else{
+  	return distance * 0.39370079;
+  }
+}
